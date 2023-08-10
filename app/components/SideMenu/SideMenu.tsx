f8838b40832bf3a58b52697bf09fbfb6ef3d6797
ThainazZ <thainasd@hotmail.com>
2023-08-10 13:54:53 -0300

    creating the dashboard and side bar

---
+++ app/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SideMenu = () => {
+    return(
+        <ul>
+            <li>Dashboard</li>
+            <li>Estoque</li>
+            <li>Feedbacks</li>
+            <li>Clientes</li>
+            <li>Parcerias</li>
+            <li>Fornecedores</li>
+            <li>Despesas</li>
+        </ul>
+    )
+}
+
+export default SideMenu;
